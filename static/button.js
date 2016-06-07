ae4553e8b7aa626ba9e013f457c180358473aa5b
Ajaay_AQUA-PC <ajaay@umich.edu>
2016-06-07 18:46:06 -0400

    AJAX request works

--- static/button.js
+++ static/button.js
@@ -1,32 +1,38 @@
+//Got instructions from https://wiki.base22.com/pages/viewpage.action?pageId=72942000
+//Form URL:
+//https://docs.google.com/forms/d/1IFjsbafZddKlqVogD9Rm49XIWP1mVWUGg7g0m8W0UkI/viewform
+//Sample URL:
+//https://docs.google.com/a/YOURDOMAIN.com/forms/d/15QFO2VE44-9gAwcJeTPPWvxAX7v_1Ye9qmjdX2VzLBw/formResponse
+//Replace YOURDOMAIN.com with umich.edu
 function recordRate()
 {
-    alert("Beginning of function!")
+    console.log("Beginning of function!")
     $.ajax({
-	//<iframe src="https://docs.google.com/forms/d/1IFjsbafZddKlqVogD9Rm49XIWP1mVWUGg7g0m8W0UkI/viewform?embedded=true#start=embed" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
 	//Form Key:
 	//1IFjsbafZddKlqVogD9Rm49XIWP1mVWUGg7g0m8W0UkI
-        url: "https://docs.google.com/1IFjsbafZddKlqVogD9Rm49XIWP1mVWUGg7g0m8W0UkI/formResponse",
+	url: "https://docs.google.com/a/umich.edu/forms/d/1IFjsbafZddKlqVogD9Rm49XIWP1mVWUGg7g0m8W0UkI/formResponse",
         data: {},
         type: "POST",
         dataType: "xml",
         statusCode: {
             0: function (){
                 //Success message
-		alert("Successfully sent timestamp! Code 0")
+		console.log("Successfully sent timestamp! Code 0")
             },
             200: function (){
                 //Fail Message
-		alert("Successfully sent timestamp! Code 200")
+		console.log("Successfully sent timestamp! Code 200")
             },
             400: function (){
                 //Fail Message
-		alert("Failed at sending timestamp!")
+		console.log("Failed at sending timestamp!")
             },
 	    404: function (){
                 //Fail Message
-		alert("FAILED!")
+		console.log("FAILED!")
             }
         }
     });
-    alert("Reached end of function!")
+    console.log("Reached end of function!")
 }
